fb9aa4be0d9c00319132054285f93d0f5f2d98e0
MarcosSouzaa <marcossouza.git@gmail.com>
2026-05-08 14:29:04 -0300

    Criação do PacienteController para receber os dados do paciente

--- src/main/java/med/voll/api/endereco/DadosEndereco.java
+++ src/main/java/med/voll/api/endereco/DadosEndereco.java
@@ -1,4 +1,11 @@
 package med.voll.api.endereco;
 
-public record DadosEndereco(String logradouro, String numero, String complemento, String bairro, String cidade, String UF, String CEP) {
+public record DadosEndereco(
+        String logradouro,
+        String numero,
+        String complemento,
+        String bairro,
+        String cidade,
+        String uf,
+        String cep) {
 }
--- src/main/java/med/voll/api/medico/DadosCadastroMedico.java
+++ src/main/java/med/voll/api/medico/DadosCadastroMedico.java
@@ -4,8 +4,9 @@ import med.voll.api.endereco.DadosEndereco;
 
 /**
  * Record criado para receber os campos que estão chegando da requisição como:
- * nome, email, crm que são finais(constantes)
+ * nome, email, telefone, crm que são finais(constantes)
  * Especialidade eu vou criar um ENUM
+ * endereço, vou criar um Record que poderá ser usado por outras classes
  */
 public record DadosCadastroMedico(String nome, String email, String telefone, String crm, Especialidades especialidade, DadosEndereco endereco) {
 }
